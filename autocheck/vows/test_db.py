f465b4036ad013f6585710843734005428329b2d
Hans-Thomas Mueller <htmue@me.com>
2011-05-29 13:49:21 +0200

    Some whitespace added to test cases.

--- autocheck/vows/test_db.py
+++ autocheck/vows/test_db.py
@@ -18,30 +18,37 @@ class DatabaseVows(unittest.TestCase):
     
     def test_can_connect_to_database(self):
         self.db.connect()
+        
         self.db |should| be_connected
     
     def test_can_close_database(self):
         self.db.connect()
+        
         self.db.close()
+        
         self.db |should_not| be_connected
     
     def test_can_add_test_run(self):
         self.db.add_run()
+        
         self.db.current_run_id |should| be(1)
         self.db.total_runs |should| be(1)
     
     def test_can_count_test_runs(self):
         for i in range(5):
             self.db.add_run()
+            
             self.db.current_run_id |should| be(i + 1)
             self.db.total_runs |should| be(i + 1)
     
     def test_can_load_test_run_object(self):
         before = datetime.datetime.utcnow()
         self.db.add_run()
-        run = self.db.get_run()
-        run['id'] |should| be(1)
         after = datetime.datetime.utcnow()
+        
+        run = self.db.get_run()
+        
+        run['id'] |should| be(1)
         run['started'] |should| be_greater_than(before)
         run['started'] |should| be_less_than(after)
     
@@ -57,13 +64,16 @@ class DatabaseVows(unittest.TestCase):
     
     def test_can_add_result(self):
         name = 'test_name (that.SpecialTest)'
+        
         self.add_result(name)
+        
         self.db.total_runs_by_test_name(name) |should| be(1)
     
     def test_knows_number_of_test_runs(self):
         name = 'test_name (that.SpecialTest)'
         self.add_result(name)
         self.add_result(name)
+        
         self.db.total_runs_by_test_name(name) |should| be(2)
     
     def test_knows_average_test_runtime(self):
@@ -75,7 +85,9 @@ class DatabaseVows(unittest.TestCase):
         started = finished
         finished = started + datetime.timedelta(seconds=2)
         self.add_result(name, started=started, finished=finished)
+        
         result = self.db.get_result(name)
+        
         average_time = timedelta_to_float(result['average_time'])
         average_time |should| close_to(1.5, delta=.1)
     
@@ -83,13 +95,17 @@ class DatabaseVows(unittest.TestCase):
         name = 'test_name (that.SpecialTest)'
         self.add_result(name)
         self.add_result(name)
+        
         result = self.db.get_result(name)
+        
         result['last_run_id'] |should| equal_to(self.db.current_run_id)
     
     def test_can_finish_test_run(self):
         name = 'test_name (that.SpecialTest)'
         self.add_result(name)
+        
         run = self.db.finish_run(True)
+        
         run['testsRun'] |should| be(1)
         run['wasSuccessful'] |should| be(True)
         run['errors'] |should| be(0)
@@ -108,6 +124,7 @@ class DatabaseVows(unittest.TestCase):
         self.db.finish_run(True)
         self.add_result(name, result='F')
         self.db.finish_run(True)
+        
         self.db.get_last_successful_run_id() |should| be(1)
     
     def test_handles_last_successful_run_id_edge_case_empty(self):
@@ -117,13 +134,16 @@ class DatabaseVows(unittest.TestCase):
         name = 'test_name (that.SpecialTest)'
         self.add_result(name, result='F')
         self.db.finish_run(True)
+        
         self.db.get_last_successful_run_id() |should| be(None)
     
     def test_can_clean_history(self):
         name = 'test_name (that.SpecialTest)'
         self.add_result(name)
         self.add_result(name)
+        
         self.db.clean_history()
+        
         self.db.total_runs |should| be(1)
     
     def test_cleans_history_when_finishing_run(self):
@@ -132,6 +152,7 @@ class DatabaseVows(unittest.TestCase):
         self.db.finish_run(True)
         self.add_result(name)
         self.db.finish_run(True)
+        
         self.db.total_runs |should| be(1)
     
     def test_does_not_clean_history_after_unsuccessful_run(self):
@@ -140,6 +161,7 @@ class DatabaseVows(unittest.TestCase):
         self.db.finish_run(True)
         self.add_result(name, result='F')
         self.db.finish_run(True)
+        
         self.db.total_runs |should| be(2)
     
     def test_does_not_clean_history_after_partial_run(self):
@@ -148,18 +170,21 @@ class DatabaseVows(unittest.TestCase):
         self.db.finish_run(True)
         self.add_result(name, result='.')
         self.db.finish_run(False)
+        
         self.db.total_runs |should| be(2)
     
     def test_keeps_track_of_full_test_runs(self):
         name = 'test_name (that.SpecialTest)'
         self.add_result(name)
         run = self.db.finish_run(True)
+        
         run['full'] |should| be(True)
     
     def test_keeps_track_of_partial_test_runs(self):
         name = 'test_name (that.SpecialTest)'
         self.add_result(name)
         run = self.db.finish_run(False)
+        
         run['full'] |should| be(False)
     
     def test_knows_id_of_last_run(self):
@@ -167,6 +192,7 @@ class DatabaseVows(unittest.TestCase):
             name = 'test_name_%d (that.SpecialTest)' % d
             self.add_result(name)
             run = self.db.finish_run(True)
+        
         self.db.get_last_run_id() |should| equal_to(run['id'])
     
     def test_knows_id_of_last_successful_full_run(self):
@@ -177,6 +203,7 @@ class DatabaseVows(unittest.TestCase):
         run_2 = self.db.finish_run(False)
         self.add_result(name % 3, result='F')
         run_3 = self.db.finish_run(True)
+        
         self.db.get_last_successful_full_run_id() |should| equal_to(run_1['id'])
     
     def test_can_collect_tests_with_certain_result_after_certain_run(self):
